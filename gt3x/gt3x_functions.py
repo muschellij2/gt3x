9f9527b09dd65fb1dd9c0ec73d50d80f01a0df80
muschellij2 <muschellij2@gmail.com>
2020-08-31 16:59:41 -0400

    should break if too long

--- gt3x/gt3x_functions.py
+++ gt3x/gt3x_functions.py
@@ -597,9 +597,12 @@ def extract_activity(log_bin, n_samples, acceleration_scale, sample_rate, use_sc
 			bits_list = []
 			for i in range(0,len(payload_bits),12):
 				
+				if (i + 12) > len(payload_bits):
+					print("i is outside payload bits = " + str(i + 12))
+					break
+
 				# extract the 12 bit as a string
 				bitstring = payload_bits[i:i+12]
-
 				# convert to 12bit two's complement to signed integer value: also store values in dictionary for faster reading if not already present (the Bits function is not as fast as reading it from a dictionary)
 				if bitstring not in bit12_to_int:
 					# convert 12bit to signed integer
