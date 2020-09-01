04e04951973d1dd2da27c44b7d0c9acff3f7fc95
muschellij2 <muschellij2@gmail.com>
2020-08-31 21:44:30 -0400

    updated gt3x

--- gt3x/gt3x_functions.py
+++ gt3x/gt3x_functions.py
@@ -676,8 +676,8 @@ def extract_activity(log_bin, n_samples, acceleration_scale, sample_rate, use_sc
 		print("reordering to X Y Z")
 
 	log_data[:,[0, 1]] = log_data[:,[1, 0]]
-	log_data = log_data[range(0, est_n_samples), ]
-	time_data = time_data[range(0, est_n_samples)]
+	log_data = log_data[range(0, est_n_samples), :]
+	time_data = time_data[range(0, est_n_samples), :]
 
 
 	return log_data, time_data
